a7669e5178a8a7b7105d29143626f61c3aa8531f
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-10-29 11:10:36 -0300

    Cadastro de fazenda funcional

--- frontend/src/views/farm-register/FarmRegister.ts
+++ frontend/src/views/farm-register/FarmRegister.ts
@@ -1,4 +1,6 @@
-import { baseApiUrl } from "@/global";
+import { success } from "@/config/toasted";
+import { baseApiUrl, showError } from "@/global";
+import store from "@/store";
 import axios from "axios";
 import { Options, Vue } from "vue-class-component";
 import { Button, Input, SelectBox, ImageBox } from '../../components'
@@ -8,7 +10,9 @@ import { Button, Input, SelectBox, ImageBox } from '../../components'
     components: { Button, Input, SelectBox, ImageBox }
 })
 export default class FarmRegister extends Vue {
-    farm: any = {}
+    farm: any = {
+        idt_user: store.getters.getUser.idt_user
+    }
     options: any = [ // Conteudo Temporario
         { id: 'Leite', value: 'Gado de Leite'},
         { id: 'Corte', value: 'Gado de Corte'},
@@ -16,12 +20,15 @@ export default class FarmRegister extends Vue {
     ]
 
     register(): void {
-        const url = `${ baseApiUrl }/<colocar endpoint aqui>`
+        delete this.farm.type // Temporario
+        
+        const url = `${ baseApiUrl }/farm`
         axios.post(url, this.farm)
             .then(() => {
+                success()
                 this.resetFields()
             })
-            .catch()
+            .catch(showError)
     }
 
     resetFields(): void {
