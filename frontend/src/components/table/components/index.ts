425433cc481430a60be4190f166867942d976192
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-11-17 11:36:15 -0300

    Update index.ts

--- frontend/src/components/table/components/index.ts
+++ frontend/src/components/table/components/index.ts
@@ -1,4 +1,5 @@
 import TablePagination from './TablePagination.vue'
 import TableSelect from './TableSelect.vue'
+import TableFilter from './TableFilter.vue'
 
-export { TablePagination, TableSelect }
+export { TablePagination, TableSelect, TableFilter }
